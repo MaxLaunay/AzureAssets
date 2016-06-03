c85afa0e02b7a383be6e695e5a1c6be0f297e19b
MaximeLaunay <maxime.launay@gmail.com>
2016-06-03 11:50:57 +0200

    Ajout repertoire scripts
    update du fichier zip.ps1

--- VSTSBuild/PHP_Site/zip.ps1
+++
@@ -1,7 +0,0 @@
-﻿Add-Type -Assembly System.IO.Compression.FileSystem
-$sourcedir = (get-location).Path
-$destFile = $sourcedir + "\..\phpsite.zip"
-   $compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
-   write-host $destFile
-  [System.IO.Compression.ZipFile]::CreateFromDirectory($sourcedir,
-        $destFile, $compressionLevel, $false)
---
+++ VSTSBuild/PSScripts/zip.ps1
@@ -0,0 +1,10 @@
+﻿Add-Type -Assembly System.IO.Compression.FileSystem
+$sourceDir = $PSScriptRoot + "\..\PHP_Site"
+$destFile = $PSScriptRoot + "\..\phpsite.zip"
+   $compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
+   write-host $destFile
+   if(test-path $destFile){
+    remove-item $destFile -Force
+   }
+  [System.IO.Compression.ZipFile]::CreateFromDirectory($sourceDir,
+        $destFile, $compressionLevel, $false)
